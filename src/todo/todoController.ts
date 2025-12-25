a73fa7926897bbd85ef9ac37d3c56c316a3d8f5a
binayakbhandari <binayakbhandari60@gmail.com>
2025-12-25 06:01:20 +0545

    handel add

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,12 +1,35 @@
+import type { Socket } from "socket.io";
 import { getSocketIO } from "../../server.js";
+import todoModel from "./todoModel.js";
 
 
 class Todo {
-    private io = getSocketIO() 
+    private io = getSocketIO()
     constructor() {
-        this.io.on("connection", (socket) => {
-            console.log("New client connected:", socket.id);
-        });
+        this.io.on("connection", (socket: Socket) => {
+            console.log("New client connected:", socket.id)
+            socket.on("addTodo", (data) => this.handleAddTodo(socket, data))
+        })
+    }
+    private async handleAddTodo(socket: Socket, data: any) {
+        try {
+            const { task, deadline, status } = data
+            await todoModel.create({
+                task,
+                deadline,
+                status
+            })
+            const todos = await todoModel.find()
+            socket.emit("todos_updated", {
+                status: "success",
+                data: todos
+            })
+        } catch (error) {
+            socket.emit("todos_updated", {
+                status: "error",
+                error
+            })
+        }
     }
 }
 
